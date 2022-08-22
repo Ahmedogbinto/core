6b183feb6c5792160f8a39bd2a79317e00d45b63
AhmedOg <ahmedogbinto@gmail.com>
2022-08-22 17:14:07 -0400

    End

--- target/generated-sources/annotations/com/mycompany/tennis/core/entity/Epreuve_.java
+++ target/generated-sources/annotations/com/mycompany/tennis/core/entity/Epreuve_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-19T19:24:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-22T17:06:47")
 @StaticMetamodel(Epreuve.class)
 public class Epreuve_ { 
 
--- target/generated-sources/annotations/com/mycompany/tennis/core/entity/Tournoi_.java
+++ target/generated-sources/annotations/com/mycompany/tennis/core/entity/Tournoi_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-19T19:24:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-08-22T17:06:47")
 @StaticMetamodel(Tournoi.class)
 public class Tournoi_ { 
 
